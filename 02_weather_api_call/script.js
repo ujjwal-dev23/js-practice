38683d3a81c136119b9ef14012c26281e19b51ae
Ujjwal <ujjwalkandera23@tutamail.com>
2025-04-04 08:08:08 +0530

    weather api project done

--- 02_weather_api_call/script.js
+++ 02_weather_api_call/script.js
@@ -9,25 +9,26 @@ document.addEventListener("DOMContentLoaded", () => {
   async function fetchWeatherData(city) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`)
     if (!response.ok) {
-      console.error("API Call didn't respond correctly")
-      return
+      throw new Error("API Response Failure")
     }
     return await response.json()
   }
 
   function displayWeatherData(city, temp, desc) {
     const cityName = document.getElementById("city-name")
-    const temprature = document.getElementById("temprature")
+    const temperature = document.getElementById("temperature")
     const description = document.getElementById("description")
 
     cityName.textContent = city
-    temprature.textContent = temp
+    temperature.textContent = temp
     description.textContent = desc
 
     weatherInfo.classList.remove("hidden")
+    errorMsg.classList.add("hidden")
   }
   
   submitBtn.addEventListener("click", async () => {
+    if (cityInput.value === "") return
     const city = cityInput.value.trim()
     try {
       const {name, main, weather} = await fetchWeatherData(city)
@@ -37,8 +38,6 @@ document.addEventListener("DOMContentLoaded", () => {
       weatherInfo.classList.add("hidden")
       errorMsg.classList.remove("hidden")
     }
-
-    //console.log(name, main.temp, weather[0].description)
 
     cityInput.value = ""
   })
